5dbf9ba3dc0b7c2277be54b9f7d834988636cea0
zheng peng <pengzheng@huishoubao.com.cn>
2022-12-08 09:17:01 +0800

    optimize

--- api_test.go
+++ api_test.go
@@ -97,7 +97,7 @@ func TestAPI(t *testing.T) {
 	fmt.Println(out)
 }
 
-func TestAPI2(t *testing.T) {
+func TestAPIMemory(t *testing.T) {
 	route := "/api/1/hello"
 	method := "POST"
 	api := &API{
@@ -156,7 +156,7 @@ func TestAPI2(t *testing.T) {
 	*/
 	tpl := template.NewTemplate(sqltpl.TemplatefuncMap)
 	tpl.AddTpl("", tplStr) //memorySource 使用
-	//tplNames := tpl.AddTpl("", tplStr)
+	tplNames := tpl.AddTpl("", tplStr)
 	capi.WithTemplate(tpl.Template)
 	/* 	var dbConfig db.DBExecProviderConfig
 	   	err = json.Unmarshal([]byte(sourceConfig), &dbConfig)
@@ -167,11 +167,11 @@ func TestAPI2(t *testing.T) {
 	*/
 	selectSQL := "select * from component where 1=1   and deleted_at is null order by updated_at desc limit 0,20 ;"
 	countSQL := "select count(*) as count from component where 1=1   and deleted_at is null;"
-	memorySource := MemorySource{}
-	memorySource.InOutMap[selectSQL] = "[{\"content\":\"\",\"created_at\":\"2022-11-17 22:03:11\",\"deleted_at\":\"\",\"description\":\"支持微信、支付宝接入,封装了异常重试、对账等功能,拿来即可使用\",\"icon\":\"flaticon-research-1\",\"id\":\"6\",\"key\":\"pay\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another6.jpg\",\"title\":\"支付组件\",\"updated_at\":\"2022-11-17 22:03:23\"},{\"content\":\"\",\"created_at\":\"2022-11-17 22:01:43\",\"deleted_at\":\"\",\"description\":\"状态是实体常有的重要属性之一,有限状态机巧妙的化解了状态变迁的难点,为程序健壮提供有力保障\",\"icon\":\"flaticon-branding\",\"id\":\"5\",\"key\":\"fsm\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another5.jpg\",\"title\":\"有限状态机\",\"updated_at\":\"2022-11-17 22:02:05\"},{\"content\":\"\",\"created_at\":\"2022-11-17 21:59:47\",\"deleted_at\":\"\",\"description\":\"接口参数校验,是频繁而无技术挑战的必备工作,经过大量总结后,采用合适的标准,构造通用参数校验组件,极大提升接口的内聚性和一致性,并将其交给机器员工,极大的减少了人为工作量\",\"icon\":\"flaticon-education\",\"id\":\"4\",\"key\":\"validator\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another4.jpg\",\"title\":\"参数校验组件\",\"updated_at\":\"2022-11-17 22:00:44\"},{\"content\":\"\",\"created_at\":\"2022-11-17 21:58:58\",\"deleted_at\":\"\",\"description\":\"excel导出功能非常常见,看似导出数据各异,经过深入思考后抽离共性,实现稳定通用的导出组件\",\"icon\":\"flaticon-web-programming\",\"id\":\"3\",\"key\":\"excel\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another3.jpg\",\"title\":\"excel导出组件\",\"updated_at\":\"2022-11-17 21:59:15\"},{\"content\":\"\",\"created_at\":\"2022-11-17 21:53:42\",\"deleted_at\":\"\",\"description\":\"账户组件包含用户登录、注册、认证、找回密码、鉴权、强安全校验等基础功能.支持auth2.0,openID等国际标准\",\"icon\":\"flaticon-research-1\",\"id\":\"2\",\"key\":\"account\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another2.jpg\",\"title\":\"账户组件\",\"updated_at\":\"2022-11-17 21:54:42\"},{\"content\":\"\",\"created_at\":\"2022-11-17 21:51:27\",\"deleted_at\":\"\",\"description\":\"地址库包含省市区数据维护和应用,用户快递收货地址,用户可以是人、商户或者公司\",\"icon\":\"flaticon-branding\",\"id\":\"1\",\"key\":\"address\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another1.jpg\",\"title\":\"地址库\",\"updated_at\":\"2022-11-17 21:53:08\"}]"
-	memorySource.InOutMap[countSQL] = "6"
-	for k := range memorySource.InOutMap {
-		capi.WithSource(k, &memorySource)
+	source := MemorySource{InOutMap: make(map[string]string)}
+	source.InOutMap[selectSQL] = "[{\"content\":\"\",\"created_at\":\"2022-11-17 22:03:11\",\"deleted_at\":\"\",\"description\":\"支持微信、支付宝接入,封装了异常重试、对账等功能,拿来即可使用\",\"icon\":\"flaticon-research-1\",\"id\":\"6\",\"key\":\"pay\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another6.jpg\",\"title\":\"支付组件\",\"updated_at\":\"2022-11-17 22:03:23\"},{\"content\":\"\",\"created_at\":\"2022-11-17 22:01:43\",\"deleted_at\":\"\",\"description\":\"状态是实体常有的重要属性之一,有限状态机巧妙的化解了状态变迁的难点,为程序健壮提供有力保障\",\"icon\":\"flaticon-branding\",\"id\":\"5\",\"key\":\"fsm\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another5.jpg\",\"title\":\"有限状态机\",\"updated_at\":\"2022-11-17 22:02:05\"},{\"content\":\"\",\"created_at\":\"2022-11-17 21:59:47\",\"deleted_at\":\"\",\"description\":\"接口参数校验,是频繁而无技术挑战的必备工作,经过大量总结后,采用合适的标准,构造通用参数校验组件,极大提升接口的内聚性和一致性,并将其交给机器员工,极大的减少了人为工作量\",\"icon\":\"flaticon-education\",\"id\":\"4\",\"key\":\"validator\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another4.jpg\",\"title\":\"参数校验组件\",\"updated_at\":\"2022-11-17 22:00:44\"},{\"content\":\"\",\"created_at\":\"2022-11-17 21:58:58\",\"deleted_at\":\"\",\"description\":\"excel导出功能非常常见,看似导出数据各异,经过深入思考后抽离共性,实现稳定通用的导出组件\",\"icon\":\"flaticon-web-programming\",\"id\":\"3\",\"key\":\"excel\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another3.jpg\",\"title\":\"excel导出组件\",\"updated_at\":\"2022-11-17 21:59:15\"},{\"content\":\"\",\"created_at\":\"2022-11-17 21:53:42\",\"deleted_at\":\"\",\"description\":\"账户组件包含用户登录、注册、认证、找回密码、鉴权、强安全校验等基础功能.支持auth2.0,openID等国际标准\",\"icon\":\"flaticon-research-1\",\"id\":\"2\",\"key\":\"account\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another2.jpg\",\"title\":\"账户组件\",\"updated_at\":\"2022-11-17 21:54:42\"},{\"content\":\"\",\"created_at\":\"2022-11-17 21:51:27\",\"deleted_at\":\"\",\"description\":\"地址库包含省市区数据维护和应用,用户快递收货地址,用户可以是人、商户或者公司\",\"icon\":\"flaticon-branding\",\"id\":\"1\",\"key\":\"address\",\"label\":\"\",\"thumb\":\"../assets/images/case-studies/case-studies-another1.jpg\",\"title\":\"地址库\",\"updated_at\":\"2022-11-17 21:53:08\"}]"
+	source.InOutMap[countSQL] = "6"
+	for _, k := range tplNames {
+		capi.WithSource(k, &source)
 	}
 
 	container := NewContainer()
